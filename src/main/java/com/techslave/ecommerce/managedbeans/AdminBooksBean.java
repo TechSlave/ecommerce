1429bead5fac9dd274907cb666a791d4d26d767a
Itamar <itamarportela@gmail.com>
2019-10-16 12:14:10 -0300

    Syncing

--- src/main/java/com/techslave/ecommerce/managedbeans/AdminBooksBean.java
+++ src/main/java/com/techslave/ecommerce/managedbeans/AdminBooksBean.java
@@ -1,5 +1,8 @@
 package com.techslave.ecommerce.managedbeans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -11,17 +14,34 @@ import com.techslave.ecommerce.models.Book;
 
 @Model
 public class AdminBooksBean {
+	
+	private List<Integer> selectedAuthorsIds = new ArrayList<>();
 
 	private Book product = new Book();
 	
 	@Inject
 	private BookDAO bookDAO = new BookDAO();
 	
+	private void populateBookAuthor() {
+		selectedAuthorsIds.stream().map( (id) -> { 
+			return new Author(id);
+			}).forEach(product _ add);
+		}
+	
+	
 	@Transactional
 	public void save() {
+		populateBookAuthor();
 		bookDAO.save(product);
 	}
 	public Book getProduct() {
 		return product;
 		}
+	public List<Integer> getSelectedAuthorsIds() {
+		return selectedAuthorsIds;
+	}
+	
+	public void setSelectedAuthorsIds(List<Integer> selectedAuthorsIds) {
+		this.selectedAuthorsIds = selectedAuthorsIds;
+	}
 }
